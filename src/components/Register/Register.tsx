afc9fdcd4c752eac01407ba721a8af28a82ab4f5
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-01-17 04:02:49 +0100

    minor tweaks

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useImage } from 'react-image';
+import logo from '../Images/logo.webp'
+
+const MyLogoComponent = () => {
+    const { src } = useImage({
+        srcList: logo,
+    })
+    const classList = 'w-1/3 md:w-32 lg:w-48'
+
+    return <img src={src} alt="logo" className={classList} />
+}
 
 
 const Register = () => {
 
   return (
-      <div>Register</div>
+      <div>
+          <header className='w-100 flex content-center top-0 absolute p-4'>
+              <a href={'/'}><MyLogoComponent /></a>
+          </header>
+      </div>
   );
 }
 
