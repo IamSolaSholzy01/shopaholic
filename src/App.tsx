f676e7ba346d7f6fb7887b972fe7e3f4ae3a3309
femidamilola <femi.adebayo1998@gmail.com>
2022-01-19 01:20:21 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Layout from './Layout';
-import Register from './components/Register';
+// import Register from './components/Register';
 import Home from './components/Home'
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element="{<Register />}" />
       </Routes>
     </Router>
   );
