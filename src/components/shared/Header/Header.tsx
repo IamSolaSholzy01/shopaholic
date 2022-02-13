d2fb2177f16f14cf884954ca0440ee4e4bde3873
femidamilola <femi.adebayo1998@gmail.com>
2022-02-14 00:05:36 +0100

    Fixes

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -4,6 +4,7 @@ import { MHidden } from "../../@material-extend";
 import Logo from "../../Logo";
 import { buttonStyle } from '../../styles';
 import React, { SyntheticEvent } from "react";
+import PersonIcon from "@mui/icons-material/Person";
 
 const HeaderStyle = styled('header')(({ theme }: { theme: any }) => {
   return ({
@@ -80,7 +81,7 @@ const Header = ({data}: {data: {
         variant: 'text' | 'contained' | 'outlined'
     },
     {
-        text: string,
+        text: any,
         styleList: string,
         route: string,
         click: (event: SyntheticEvent) => void,
@@ -89,18 +90,18 @@ const Header = ({data}: {data: {
   ] = [
     {
         text: 'logout',
-        styleList: 'text-gray-500 hover:text-gray-900',
+        styleList: 'text-white rounded-full',
         route: '/logout',
         click: (event: SyntheticEvent) => {
           event.preventDefault();
           data.loginLogout(event);
         },
-        variant: 'text'
+        variant: 'contained'
     },
     {
-        text: 'profile',
+        text: <PersonIcon />,
         route: '/profile',
-        styleList: 'ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700',
+        styleList: 'ml-8 inline-flex items-center justify-center px-1 py-2 border border-transparent rounded-full shadow-sm text-white',
         click: (event: SyntheticEvent) => {
           event.preventDefault();
           data.registerAuthModal(event);
@@ -115,9 +116,9 @@ const Header = ({data}: {data: {
         <Logo />
       </Link>
       <MHidden width="xsDown">
-        <Stack direction="row" spacing="1">
+        <Stack direction="row" spacing={2}>
           {(data.isLoggedIn ? loginList : list).map((item, index) => (
-            <Button key={index} color='secondary' variant={item.variant} onClick={(e) => {item.click(e)}} sx={buttonStyle}>
+            <Button key={index} color='secondary' variant={item.variant} onClick={(e) => {item.click(e)}} sx={buttonStyle} className={item.styleList}>
               {item.text}
             </Button>
           ))}
